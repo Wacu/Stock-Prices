966161d41d3e227f2dbc99f37c75a3a58b55de4b
Wacu_Mutahi <28296097+Wacu@users.noreply.github.com>
2023-05-18 16:12:37 +0300

    Add files via upload

---
+++ StockPrices.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+
+st.write("""
+
+# A simple Stock Price Application
+
+We have the *Stock closing price* and the *volume* of Google!
+**Enjoy**
+
+""" )
+
+#Define the ticker symbol
+tickerSymbol = 'GOOGL'
+
+#Get data on this ticker
+tickerData=yf.Ticker(tickerSymbol)
+
+#Get historical prices for this ticker
+tickerDf=tickerData.history(persiod='1d',start='2010-5-31',end='2020-5-31')
+
+#Variables obtained
+#Open High Low Close Volume Dividents Stock Splits
+
+#line plots
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
+
+
